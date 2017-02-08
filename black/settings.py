62d0fbb6ff7b9bcb2edf7646acdd234385bf6999
iad <bmth@me.com>
2017-02-08 19:42:54 +0300

    Postgres integrated.

--- black/settings.py
+++ black/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'black.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'black',
+        'USER': 'black',
+        'PASSWORD': 'black101',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
