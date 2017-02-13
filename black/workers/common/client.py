9c6a79e6e3668ce67552611a6bb9a1e34e655361
iad <bmth@me.com>
2017-02-13 14:50:27 +0300

    Some more unseccessful exetcises with subprocess + py2.

--- black/workers/common/client.py
+++ black/workers/common/client.py
@@ -3,9 +3,7 @@ from kombu.pools import producers
 from queues import task_exchange, task_queue, notifications_queue
 
 
-def send_as_task(connection, value):
-    payload = {'i': value}
-
+def send_as_task(connection, payload):
     with producers[connection].acquire(block=True) as producer:
         producer.publish(payload,
                          serializer='pickle',
@@ -30,7 +28,8 @@ if __name__ == '__main__':
     from kombu import Connection
 
     connection = Connection('redis://localhost:6379/')
-    for i in xrange(0,3):
-        send_as_task(connection, i)
+    # send_as_task(connection, {'command': 'nmap', 'flags': [], 'target': 'ya.ru'})
+    # send_as_task(connection, {'command': 'nmap', 'flags': [], 'target': 'ya.ru'})
+    send_as_task(connection, {'command': 'nmap', 'flags': [], 'target': 'ya.ru'})
     for i in xrange(0,3):
         send_as_notification(connection, i)
--- black/workers/common/worker.py
+++ black/workers/common/worker.py
@@ -1,21 +1,23 @@
 from queues import task_queue, notifications_queue
 from time import sleep
+import socket
 from subprocess import Popen, PIPE
-import threading
 
 from kombu import Connection
 
 
 working_processes = []
 
-def long_function():
-    for i in xrange(0,10):
-        sleep(0.5)
-        print "     Work: {}".format(i)
 
 def process_task(body, message):
     print "[T] " + str(body)
-    p = Popen(['./test.sh'], shell=True, stdout=PIPE, stderr=PIPE)
+    # args = []
+    # args.append(body['command'])
+    # args += body['flags']
+    # args.append(body['target'])
+    # print args
+    # p = Popen(['/usr/local/bin/nmap','ya.ru'], shell=True, stdout=PIPE, stderr=PIPE)
+    p = Popen(['nmap'], shell=True, stdout=PIPE, stderr=PIPE)
     working_processes.append(p)
 
     message.ack()
@@ -26,7 +28,7 @@ def process_notification(body, message):
 
 def track_processes():
     global working_processes
-
+    print "   -> " + str(len(working_processes))
     for i in xrange(0, len(working_processes)):
         pc = working_processes[i]
 
@@ -35,6 +37,7 @@ def track_processes():
             (stdout, stderr) = pc.communicate()
             print stdout
             print stderr
+            del working_processes[i]
 
     return True
 
@@ -45,19 +48,18 @@ def start():
 
             if ready_for_task:
                 with conn.Consumer(task_queue, accept=['pickle','json'], callbacks=[process_task]) as tasks_consumer:
-                    print "-"*20
                     try:
                         conn.drain_events(timeout=2)
-                    except Exception as e:
-                        print "[T_E] " + str(e)
+                    except socket.timeout as e:
+                        print "[T_E] " + str(e.message)
+                        # raise e.message
                         pass
 
-            print "-"*20
-            with conn.Consumer(notifications_queue, accept=['pickle','json'], callbacks=[process_notification]) as notifications_consumer:
-                try:
-                    conn.drain_events(timeout=2)
-                except Exception as e:
-                    print "[N_E] " + str(e)
-                    pass
+            # with conn.Consumer(notifications_queue, accept=['pickle','json'], callbacks=[process_notification]) as notifications_consumer:
+            #     try:
+            #         conn.drain_events(timeout=2)
+            #     except Exception as e:
+            #         print "[N_E] " + str(e)
+            #         pass
 
 start()
