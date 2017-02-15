3b07c9a3f67480ff8ab89ec52091788e11f4e939
iad <bmth@me.com>
2017-02-15 11:02:46 +0300

    Added a platform check.

--- black/workers/async_test/nmap_worker.py
+++ black/workers/async_test/nmap_worker.py
@@ -1,14 +1,15 @@
-import os
 import asyncio
+import platform
 import aioredis
-from time import sleep
-from asyncio.subprocess import PIPE
 from concurrent.futures._base import TimeoutError
 
 from worker import Worker
 
+
 class NmapProcess(object):
+
     def __init__(self, process_id, command):
+        self.platform = platform.system()
         self.id = process_id
         self.command = command
         self.status = "New"
@@ -28,13 +29,24 @@ class NmapProcess(object):
     async def process_notification(self, command):
         if command == 'pause':
             print("[Notif] Pause")
-            self.proc.send_signal(17) # SIGSTOP
+
+            if self.platform == "Darwin":
+                self.proc.send_signal(17)  # SIGSTOP
+            elif self.platform == "Linux":
+                self.proc.send_signal(20)  # SIGSTOP
+            else:
+                raise Exception("Poshel nahui")
         elif command == 'stop':
             print("[Notif] Stop")
-            self.proc.terminate() # SIGTERM
+            self.proc.terminate()  # SIGTERM
         elif command == 'unpause':
             print("[Notif] Unpause")
-            self.proc.send_signal(19) # SIGCONT
+            if self.platform == "Darwin":
+                self.proc.send_signal(19)  # SIGSTOP
+            elif self.platform == "Linux":
+                self.proc.send_signal(18)  # SIGSTOP
+            else:
+                raise Exception("Poshel nahui")
 
     async def check_if_exited(self):
         try:
@@ -42,7 +54,7 @@ class NmapProcess(object):
             (stdout, stderr) = await asyncio.wait_for(self.proc.communicate(), 0.1)
         except TimeoutError as e:
             # Not yet finished
-            return False            
+            return False
         else:
             # The process have exited.
             # Save the data locally.]
@@ -57,7 +69,6 @@ class NmapProcess(object):
                 self.status = "Aborted "
 
             return True
-
 
 
 loop = asyncio.get_event_loop()
