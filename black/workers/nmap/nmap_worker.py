c1a8646e2a2e2915c792e4eb51ea23a76808a59b
iad <bmth@me.com>
2017-02-16 16:17:20 +0300

    Fixed some more deps issues.

--- black/workers/nmap/nmap_worker.py
+++ black/workers/nmap/nmap_worker.py
@@ -67,12 +67,3 @@ class NmapTask(object):
                 self.status = "Aborted"
 
             return True
-
-
-loop = asyncio.get_event_loop()
-nmap = Worker('nmap', NmapTask)
-loop.run_until_complete(nmap.initialize())
-loop.run_until_complete(nmap.start_tasks_consumer())
-loop.run_until_complete(nmap.start_notifications_consumer())
-loop.run_until_complete(nmap.update_active_processes())
-loop.run_forever()
