982948ca3ef8580f9ce7037ffef701640d3b650c
iad <bmth@me.com>
2017-02-20 16:17:57 +0300

    Producer added.

---
+++ black/workers/screenshotter/producer.py
@@ -0,0 +1,45 @@
+import asyncio
+from uuid import uuid4
+
+import asynqp
+
+
+def main():
+    loop = asyncio.get_event_loop()
+
+    async def send_screenshotter_start():
+        random_id = "screenshotter_task_"+str(uuid4())
+
+        connection = await asynqp.connect('localhost', 5672, username='guest', password='guest')
+
+        # Open a communications channel
+        channel = await connection.open_channel()
+
+        # Create 2 queues and an exchange on the broker
+        exchange = await channel.declare_exchange('tasks.exchange', 'direct')
+        tasks_queue = await channel.declare_queue('screenshotter_tasks')
+        notifications_queue = await channel.declare_queue('screenshotter_notifications')
+
+        # Bind the queue to the exchange, so the queue will get messages
+        # published to the exchange
+        await tasks_queue.bind(exchange, routing_key='screenshotter_tasks')
+        await notifications_queue.bind(exchange, routing_key='screenshotter_notifications')
+
+        print("-"*20)
+        print("Sending task")
+
+        msg = asynqp.Message({
+            'task_id': 'screenshotter_task_' + random_id,
+            'command': 'ya.ru'
+        })
+        exchange.publish(msg, 'screenshotter_tasks')
+        print("Sent task")
+
+        await channel.close()
+        await connection.close()
+
+    loop.run_until_complete(send_screenshotter_start())
+
+
+if __name__ == '__main__':
+    main()
