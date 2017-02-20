c7a0d46cd0b0f8348de8b542173dbd5566378727
iad <bmth@me.com>
2017-02-20 13:13:26 +0300

    Some more linting fixes.

--- black/workers/masscan/masscan_worker.py
+++ black/workers/masscan/masscan_worker.py
@@ -1,23 +1,15 @@
 """ Module that will accumulate all the tasks described """
-import asyncio
 from black.workers.masscan import MasscanTask
 from black.workers.common.worker import Worker
 
 
 class MasscanWorker(Worker):
-	""" """
-	def __init__(self):
-		Worker.__init__(self, 'masscan', MasscanTask)
+    """ """
+    def __init__(self):
+        Worker.__init__(self, 'masscan', MasscanTask)
 
-	async def start(self):
-		await self.initialize()
-		await self.start_tasks_consumer()
-		await self.start_notifications_consumer()
-
-
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(masscan.initialize())
-# loop.run_until_complete(masscan.start_tasks_consumer())
-# loop.run_until_complete(masscan.start_notifications_consumer())
-# loop.run_forever()
+    async def start(self):
+        """ Start all the necessary consumers """
+        await self.initialize()
+        await self.start_tasks_consumer()
+        await self.start_notifications_consumer()
