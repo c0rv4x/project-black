4588f6ba7853ecb7a61939466d2ba212f25dd1ae
iad <bmth@me.com>
2017-02-21 13:38:35 +0300

    Some additions.

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
 """ Module that will accumulate all the tasks described """
 import asyncio
-from black.workers.masscan.masscan_worker import MasscanWorker
+from black.workers.common.sync_worker import SyncWorker
+# from black.workers.masscan.masscan_worker import MasscanWorker
 
 
-loop = asyncio.get_event_loop()
-masscan = MasscanWorker()
-loop.create_task(masscan.start())
-loop.run_forever()
+# loop = asyncio.get_event_loop()
+# masscan = MasscanWorker()
+# loop.create_task(masscan.start())
+# loop.run_forever()
+
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+		
+
+w = SyncWorker('test', ClassName)
+w.initialize()
+w.start_tasks_consumer()
