1f973b255f4cbbc8596dfc4dd876a8e7db5d96c4
iad <bmth@me.com>
2017-02-22 15:06:44 +0300

    Removed Django deps.

--- black/workers/screenshotter/screenshotter_task.py
+++ black/workers/screenshotter/screenshotter_task.py
@@ -2,8 +2,6 @@
 to manager the launched instance of scan. """
 from selenium import webdriver
 
-from black.models import Project, Scan
-
 from black.workers.common.task import Task
 from .screenshot_maker import make_screenshot
 
@@ -53,7 +51,4 @@ class ScreenshotterTask(Task):
         """ Save the information to the DB. """
         # TODO: wait, wait, at which position should i save the picture?
         # Meaning, if we rescan, should save to the last one?
-        project = Project.objects.get(project_name=self.project_name)
-        print(project)
-        found_data = Scan.objects.get(project_name=self.project_name)
-        print(found_data)
+        pass
