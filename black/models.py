c848717ebf916bf7fbebfe9afbe54fd4516edf9e
iad <bmth@me.com>
2017-02-25 22:01:50 +0300

    Added a ton of comments, related to the ORM models.

--- black/models.py
+++ black/models.py
@@ -25,10 +25,14 @@ def destroy_session(session):
         session.close()
         sessions.remove(session)
 
+
 Base = declarative_base()
 
 class Project(Base):
+    """ Kepps the names of all the projects that exist
+    in the system """
     __tablename__ = 'projects'
+
     project_name = Column(String, primary_key=True)
 
     def __repr__(self):
@@ -37,12 +41,27 @@ class Project(Base):
 
 
 class Task(Base):
+    """ Keeps the data of all the tasks that ever existed
+    in the system (though not the deleted ones) """
     __tablename__ = 'tasks'
+
+    # Primary key (uuid4)
     task_id = Column(String, primary_key=True)
+
+    # Literal name of the task: dnsscan, nmap etc.
     task_type = Column(String)
+
+    # Target can be: hostname, ip, URL 
+    #    (depending on the task_type)
     target = Column(String)
+
+    # Params of the lanuched task (flags, dictionaries etc.)
     params = Column(String)
+
+    # {New, Working, Finished, Aborted, ...}
     status = Column(String)
+
+    # The name of the related project
     project_name = Column(String, ForeignKey('projects.project_name'))
 
     # def __repr__(self):
@@ -51,10 +70,23 @@ class Task(Base):
 
 
 class Scope(Base):
+    """ Kepps the data on scope:
+    * Hostnames
+    * IPs
+    * Related data (like, 'scope_id' and the project name)  """
     __tablename__ = 'scopes'
+
+    # Primary key (probably uuid4)
     scope_id = Column(String, primary_key=True)
+
+    # The hostname can be either string or None
     hostname = Column(String)
+
+    # IP address is a string (probably None, but not sure if 
+    #    is needed)
     ip_address = Column(String)
+
+    # The name of the related project
     project_name = Column(String, ForeignKey('projects.project_name'))
 
     def __repr__(self):
@@ -65,14 +97,34 @@ class Scope(Base):
 
 
 class Scan(Base):
+    """ Keep the major part of data: everything that has been
+    found during the scan """
     __tablename__ = 'scans'
-    id = Column(String, primary_key=True)
+
+    # Primary key (probably uuid4)
+    scan_id = Column(String, primary_key=True)
+
+    # Target is (probably) host/ip
     target = Column(String)
+
+    # TCP port number
     port_number = Column(Integer)
+
+    # The network protocol (tcp, udp ...) of the port
     protocol = Column(String)
+
+    # Banner that was received from Nmap
     banner = Column(String)
+
+    # Path to the screenshot image
     screenshot_path = Column(String)
+
+    # ID of the related task (the task, which resulted in the current data)
+    # TODO: should think about the following:
+    #    many scans can update one record, should we store all the ids?
     task_id = Column(String, ForeignKey('tasks.task_id')) # TODO: add on_delete
+
+    # The name of the related project
     project_name = Column(String, ForeignKey('projects.project_name'))
 
     # def __repr__(self):
