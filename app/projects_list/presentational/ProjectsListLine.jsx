d8e6c8dc3c918c2175706f8f7c640a9fefccf60e
iad <bmth@me.com>
2017-03-16 16:26:23 +0300

    Button of project del is now red.

--- app/projects_list/presentational/ProjectsListLine.jsx
+++ app/projects_list/presentational/ProjectsListLine.jsx
@@ -1,5 +1,6 @@
 import _  from 'lodash';
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 
 class ProjectsListLine extends React.Component
@@ -15,7 +16,7 @@ class ProjectsListLine extends React.Component
                 <td>{this.props.project.project_uuid}</td>
                 <td>{this.props.project.project_name}</td>
                 <td>
-                    <button onClick={() => this.props.onDelete(this.props.project.project_uuid)}>Delete</button>
+                    <Button bsStyle="danger" onClick={() => this.props.onDelete(this.props.project.project_uuid)}>Delete</Button>
                     </td>
             </tr>
         )
