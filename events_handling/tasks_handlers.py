f062c5d96e65f314fb1aef592368e2ca512f93e0
iad <bmth@me.com>
2017-03-20 13:53:00 +0300

    Tasks progress pusher is now working in another thread and in while-loop.

--- events_handling/tasks_handlers.py
+++ events_handling/tasks_handlers.py
@@ -3,18 +3,32 @@ from flask_socketio import emit
 from managers import TaskManager
 
 
+thread = None
+
 def initialize(socketio):
+    def infinite_sender_of_tasks(task_manager):
+        print("infitinite started")
+        while True:
+            socketio.sleep(0.7)
+            send_tasks_back(task_manager)
+
+    def send_tasks_back(task_manager):
+        socketio.emit('tasks:all:get:back', {
+            "status": "success", 
+            "tasks": task_manager.get_tasks_native_objects()
+        }, broadcast=True)        
+
     task_manager = TaskManager()
 
     @socketio.on('tasks:all:get')
     def handle_custom_event():
         """ When received this message, send back all the tasks """
-        emit('tasks:all:get:back', {
-            "status": "success", 
-            "tasks": task_manager.get_tasks_native_objects()
-        })
-        # task_manager.create_task()#task_type, target, params, project_uuid)
+        global thread
 
+        send_tasks_back(task_manager)
+
+        if thread is None:
+            thread = socketio.start_background_task(target=infinite_sender_of_tasks, task_manager=task_manager)
 
     @socketio.on('tasks:create')
     def handle_project_creation(msg):
