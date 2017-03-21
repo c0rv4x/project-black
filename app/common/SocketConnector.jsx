e4d8aa6451f2d8f3dfc8719d185ba2b76b22efb9
iad <bmth@me.com>
2017-03-21 16:42:02 +0300

    Connector class is now much simpler and is no longer singleton.

--- app/common/SocketConnector.jsx
+++ app/common/SocketConnector.jsx
@@ -1,34 +1,19 @@
 import io from 'socket.io-client';
 
-let instance = null;
 
 class Connector {
 
     constructor() {
-        if(!instance){
-              instance = this;
-        }
-        this.connected = false;
-
-        this.registered_event_names = [];
         this.socketio = io("http://127.0.0.1:5000/");
-
-        return instance;
     }
 
     after_connected(callback) {
-        if (!this.connected) {
-            this.socketio.on('connect', () => {
-                callback();
-            });
-        }
-        else {
-            callback()
-        }
+        this.socketio.on('connect', () => {
+            callback();
+        });
     }
 
     listen(eventName, callback) {
-        this.registered_event_names.push(eventName);
     	this.socketio.on(eventName, callback);
     }
 
