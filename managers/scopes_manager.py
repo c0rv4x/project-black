2ed88ae3240f888cbaa428cd9c7e5f2219b7123e
iad <bmth@me.com>
2017-03-27 12:23:19 +0300

    Host was incorrectly used.

--- managers/scopes_manager.py
+++ managers/scopes_manager.py
@@ -30,17 +30,16 @@ class ScopeManager(object):
         session = sessions.get_new_session()
         ips_from_db = session.query(IP_addr).all()
         self.ips = list(map(lambda x: IP(x.ip_id,
-                                               x.ip_address,
-                                               x.hostnames,
-                                               x.comment,
-                                               x.project_uuid),
+                                         x.ip_address,
+                                         x.hostnames,
+                                         x.comment,
+                                         x.project_uuid),
                             ips_from_db))
 
         hosts_from_db = session.query(HostDB).all()
         pending_hosts = list(filter(lambda x: x.ip_address == None, hosts_from_db))
         self.pending_hosts = list(map(lambda x: Host(x.host_id,
-                                                     None, # ip_address
-                                                     [x.hostname], # a list of hostnames
+                                                     x.hostname,
                                                      x.comment,
                                                      x.project_uuid),
                                       pending_hosts))
