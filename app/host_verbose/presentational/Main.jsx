0854db45f8433aaecaac51361dcd1ec508eff858
iad <bmth@me.com>
2017-04-03 19:03:29 +0300

    Added an initial page for verbose hosts page.

---
+++ app/host_verbose/presentational/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+
+class Main extends React.Component {
+	cosntructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div></div>
+		)
+	}
+}
