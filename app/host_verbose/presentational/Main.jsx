8407d2bb82357dc68b9ddcf695f8ad23e50266cf
iad <bmth@me.com>
2017-04-11 15:00:41 +0300

    Added buttons for launching tasks.

--- app/host_verbose/presentational/Main.jsx
+++ app/host_verbose/presentational/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import ScopeComment from '../../project_details/presentational/scope/ScopeComment.jsx'
 import PortsTabs from './PortsTabs.jsx'
+import TasksButtons from './TasksButtons.jsx'
+
 
 class Main extends React.Component {
 	constructor(props) {
@@ -11,7 +13,7 @@ class Main extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2>{this.props.host.hostname}</h2>
+				<h2>{this.props.host.hostname} <TasksButtons /></h2>
 				<hr />
 				<ScopeComment commentValue={this.props.host.comment} />
 
