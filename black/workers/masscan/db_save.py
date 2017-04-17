64fae82aa256f079aeca6c66cfbf970a0138d003
iad <bmth@me.com>
2017-04-17 14:39:19 +0300

    Fixed a bug in masscan.

--- black/workers/masscan/db_save.py
+++ black/workers/masscan/db_save.py
@@ -14,9 +14,31 @@ def save_raw_output(task_id, output, project_uuid):
 
 			open_ports = dict()
 			session = sessions.get_new_session()
-			for each_host in parsed_dict['nmaprun']['host']:
-				address = each_host['address']['@addr']
 
+			if isinstance(parsed_dict['nmaprun']['host'], list):
+				for each_host in parsed_dict['nmaprun']['host']:
+					address = each_host['address']['@addr']
+					port_data = each_host['ports']['port']
+
+					protocol = port_data['@protocol']
+					port_number = int(port_data['@portid'])
+
+					port_state = port_data['state']
+					port_status = port_state['@state']
+
+					if port_state != 'closed':
+						new_scan = Scan(
+							scan_id=str(uuid4()),
+							target=address,
+							port_number=port_number,
+							task_id=task_id,
+							project_uuid=project_uuid)
+
+						session.add(new_scan)
+						session.commit()
+			else:
+				each_host = parsed_dict['nmaprun']['host']
+				address = each_host['address']['@addr']
 				port_data = each_host['ports']['port']
 
 				protocol = port_data['@protocol']
@@ -34,7 +56,7 @@ def save_raw_output(task_id, output, project_uuid):
 						project_uuid=project_uuid)
 
 					session.add(new_scan)
-					session.commit()
+					session.commit()				
 
 			sessions.destroy_session(session)
 
