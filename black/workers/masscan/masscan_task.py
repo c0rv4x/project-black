7537d92fc8e4cb5ec5dbe96393a1ad3a5db46a5f
iad <bmth@me.com>
2017-05-05 17:59:33 +0300

    Changed a way masscan parses options.

--- black/workers/masscan/masscan_task.py
+++ black/workers/masscan/masscan_task.py
@@ -29,13 +29,7 @@ class MasscanTask(AsyncTask):
 
     async def start(self):
         """ Launch the task and readers of stdout, stderr """
-        parsed_params = list()
-
-        for key in self.params['program']:
-            parsed_params.append(key)
-            parsed_params.append(self.params['program'][key])
-
-        self.command = ['sudo', 'masscan'] + [self.target] + ['-oX', '-'] + parsed_params
+        self.command = ['sudo', 'masscan'] + [self.target] + ['-oX', '-'] + self.params['program']
 
         try:
             self.proc = await asyncio.create_subprocess_exec(*self.command, stdout=PIPE, stderr=PIPE)
