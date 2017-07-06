211b7c96b87e6c789b02d99652f90b3e2a52fa11
iad <bmth@me.com>
2017-07-06 18:54:37 +0300

    Added TasksButtonsTracked file.

---
+++ app/hosts_list/components/TasksButtonsTracked.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import TasksSocketioEventsEmitter from '../../redux/tasks/TasksSocketioEventsEmitter.js'
+import ButtonsTasks from './ButtonsTasks.jsx'
+
+
+class TasksButtonsTracked extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.tasksEmitter = new TasksSocketioEventsEmitter();
+
+		this.dirbusterStart = this.dirbusterStart.bind(this);
+	}
+
+	dirbusterStart(options) {
+		this.tasksEmitter.requestCreateTask('dirsearch', 
+											[scheme + "://" + target], 
+											{'program': options}, 
+											this.props.project.project_uuid)
+	}
+
+	render() {
+		return (
+			<ButtonsTasks tasks={
+				[
+					{
+						"name": "Dirbuter",
+						"handler": this.dirbusterStart,
+						"preformed_options": [
+							{
+								"name": "PHP fanboy",
+								"options": {
+									"extensions": "php,php5,phps,php.bak",
+									"path": "/"
+								}
+							},
+							{
+								"name": "ASP faggot",
+								"options": {
+									"extensions": "asp",
+									"path": "/"
+								}
+							},
+							{
+								"name": "Personal favourites",
+								"options": {
+									"extensions": "php,asp,txt,conf,log,bak",
+									"path": "/"
+								}
+							}
+						],
+						"available_options": [
+							{
+								"name": "path",
+								"type": "text",
+								"default_value": "/"
+							},						
+							{
+								"name": "extensions",
+								"type": "text",
+								"default_value": "txt,conf,log,bak"
+							},
+							{
+								"name": "recursive",
+								"type": "checkbox",
+								"default_value": true
+							}
+						]
+					}
+				]
+			} />
+		)
+	}
+}
+
+export default TasksButtonsTracked;
