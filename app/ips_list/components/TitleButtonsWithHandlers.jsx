9d516012668902a0df91d675f779f31b1bb2e679
iad <bmth@me.com>
2017-07-11 19:51:10 +0300

    Masscan options now have rate by default.

--- app/ips_list/components/TitleButtonsWithHandlers.jsx
+++ app/ips_list/components/TitleButtonsWithHandlers.jsx
@@ -87,11 +87,15 @@ class TitleButtonsWithHandlers extends React.Component {
 									"preformed_options": [
 										{
 											"name": "All Ports",
-											"options": "-p1-65535"
+											"options": "--rate 10000 -p1-65535"
 										},
 										{
-											"name": "Top 1000 ports",
-											"options": "-p80,23,443,21,22,25,3389,110,445,139,143,53,135,3306,8080,1723,111,995,993,5900,1025,587,8888,199,1720,113,554,256"
+											"name": "10k Ports",
+											"options": "--rate 10000 -p1-10000"
+										},										
+										{
+											"name": "Top N ports",
+											"options": "--rate 10000 -p80,23,443,21,22,25,3389,110,445,139,143,53,135,3306,8080,1723,111,995,993,5900,1025,587,8888,199,1720,113,554,256"
 										}
 									]
 						  		},
