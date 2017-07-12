055303680d338fcdd9eea8a2149f89bb0d4de2c4
iad <bmth@me.com>
2017-07-12 11:57:52 +0300

    Added basic authentication for all urls.

--- app.py
+++ app.py
@@ -8,6 +8,32 @@ from flask_socketio import SocketIO
 
 from events_handling import Handlers
 
+from functools import wraps
+from flask import request, Response
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'pt' and password == 'blackisbackfuckers'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 # Define Flask app and wrap it into SocketIO
 app = Flask(__name__)
@@ -24,16 +50,19 @@ app.url_map.converters['regex'] = RegexConverter
 
 
 @app.route('/')
+@requires_auth
 def send_main():
     """ Simple server of statics """
     return send_from_directory('public', 'index.html')
 
 @app.route('/<regex(".*[^\.js]"):path>')
+@requires_auth
 def send_not_js(path):
     """ Simple server of statics """
     return send_from_directory('public', 'index.html')
 
 @app.route('/<regex(".*\.js"):path>')
+@requires_auth
 def send_js(path):
     """ Simple server of statics """
     return send_from_directory('public', path)
@@ -42,4 +71,4 @@ def send_js(path):
 h = Handlers(socketio)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
