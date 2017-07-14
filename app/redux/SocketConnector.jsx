135c8e2b05f7f5b4e83f05c70865370ef6748ecd
iad <bmth@me.com>
2017-07-14 13:13:44 +0300

    SocketIO address is changed dynamically.

--- app/redux/SocketConnector.jsx
+++ app/redux/SocketConnector.jsx
@@ -11,7 +11,7 @@ class Connector {
         }
         else {
             this.name = name;
-            this.socketio = io("http://31.44.88.173:5000/" + name);  
+            this.socketio = io("http://" + location.host + "/" + name);  
             connections[name] = this.socketio;
         }
     }
