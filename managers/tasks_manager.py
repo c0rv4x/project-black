c1c0f05fe833a71755f200e5bd8127fe47097c0a
iad <bmth@me.com>
2017-08-24 10:39:58 +0300

    Fixed bug.

--- managers/tasks_manager.py
+++ managers/tasks_manager.py
@@ -170,7 +170,7 @@ class TaskManager(object):
         except Exception as e:
             message = json.loads(message)
         task_id = message['task_id']
-        print("Message:",message, list(self.active_tasks.map(lambda x: x.task_id)))
+        print("Message:",message, list(map(lambda x: x.task_id, self.active_tasks)))
 
         for task in self.active_tasks:
             if task.task_id == task_id:
