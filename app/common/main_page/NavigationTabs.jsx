e802d5577c2e0eb8d148b7a4119b3e4d2bcd9f98
iad <bmth@me.com>
2017-08-25 16:28:48 +0300

    One more test.

--- app/common/main_page/NavigationTabs.jsx
+++ app/common/main_page/NavigationTabs.jsx
@@ -44,8 +44,8 @@ class NavigationTabs extends React.Component {
 				<Tab eventKey={1} title="Scope Setup">
 					<ScopeSetupWrapper project_uuid={this.project_uuid} />
 				</Tab>
-				<Tab eventKey={2} title="IPs">
-					<ProjectDetailsWrapper project_uuid={this.project_uuid} />
+				<Tab eventKey={3} title="Hostnames">
+					<HostsListWrapper project_uuid={this.project_uuid} />
 				</Tab>
 				<Tab eventKey={4} title="All Tasks">
 					<TasksTabWrapper project_uuid={this.project_uuid} />
