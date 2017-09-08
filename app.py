b395d42ff39892288430f08ed75249d8128fdf33
iad <bmth@me.com>
2017-09-08 14:28:05 +0300

    Added basic auth for all pages

--- app.py
+++ app.py
@@ -1,15 +1,64 @@
 """ Main application, running on sanic """
 import os
-import socketio
+import base64
+from functools import wraps
 
+import socketio
 from sanic import Sanic, response
 
 from events_handling import Handlers
 
 
-async def cb_complex_handler(request, project_uuid):
+def check_authorization(request):
+    """ Check if authed """
+    if request.token:
+        encoded = request.token.split(' ')[1]
+        authentication = base64.b64decode(encoded).decode('utf-8')
+
+        login = authentication.split(':')[0]
+        password = authentication.split(':')[1]
+
+        if login == 'pt' and password == 'blackisbackfuckers':
+            return True
+
+    return False
+
+
+def authorized():
+    def decorator(func):
+        @wraps(func)
+        async def decorated_function(request, *args, **kwargs):
+            # run some method that checks the request
+            # for the client's authorization status
+            is_authorized = check_authorization(request)
+
+            if is_authorized:
+                # the user is authorized.
+                # run the handler method and return the response
+                resp = await func(request, *args, **kwargs)
+                return resp
+
+            # the user is not authorized.
+            return response.json(
+                {'message': 'Not Authenticated'},
+                headers={'WWW-Authenticate': 'Basic realm="Login Required"'},
+                status=401
+            )
+        return decorated_function
+    return decorator
+
+
+@authorized()
+async def cb_complex_handler(request, project_uuid=None):
     """ Simlpy returns index.html """
     return await response.file_stream(os.path.abspath('./public/index.html'))
+
+@authorized()
+async def cb_complex_handler_bundle(request):
+    """ Simlpy returns bundle.js """
+    return await response.file_stream(os.path.abspath('./public/bundle.js'))
+
+
 
 
 SOCKET_IO = socketio.AsyncServer(async_mode='sanic')
@@ -17,8 +66,8 @@ APP = Sanic()
 
 SOCKET_IO.attach(APP)
 
-APP.static('/', './public/index.html')
-APP.static('/bundle.js', './public/bundle.js')
+APP.add_route(cb_complex_handler, '/')
+APP.add_route(cb_complex_handler_bundle, '/bundle.js')
 APP.add_route(cb_complex_handler, '/project/<project_uuid>')
 
 Handlers(SOCKET_IO, APP)
