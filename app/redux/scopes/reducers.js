6bce7f62f758e1f7093dc116d7b9fae625bd56fb
iad <bmth@me.com>
2017-10-03 13:47:09 +0300

    All scope reducers are now filtering events, based on the project_uuid, which was received in the message

--- app/redux/scopes/reducers.js
+++ app/redux/scopes/reducers.js
@@ -22,8 +22,6 @@ function create_scope(state = initialState, action) {
 		if (message["status"] == 'success') {
 			var new_state = JSON.parse(JSON.stringify(state));
 
-			console.log("Scopes before creating:", new_state['ips']);
-
 			var new_ips = _.filter(message["new_scopes"], (x) => {
 				return x['type'] == 'ip';
 			})
@@ -61,29 +59,30 @@ function create_scope(state = initialState, action) {
 function delete_scope(state = initialState, action) {
 	const message = action.message;
 
-	if (message["status"] == 'success') {
-		var ips_filtered = _.filter(state['ips'], (x) => {
-			return x["_id"] != message["_id"];
-		});
-		var hosts_filtered = _.filter(state['hosts'], (x) => {
-			return x["_id"] != message["_id"];
-		});
+	if (action.current_project_uuid !== message.project_uuid) { return state; }
+	else {
+		if (message["status"] == 'success') {
+			var ips_filtered = _.filter(state['ips'], (x) => {
+				return x["_id"] != message["_id"];
+			});
+			var hosts_filtered = _.filter(state['hosts'], (x) => {
+				return x["_id"] != message["_id"];
+			});
 
-		var state_new = {
-			'ips': ips_filtered,
-			'hosts': hosts_filtered
+			var state_new = {
+				'ips': ips_filtered,
+				'hosts': hosts_filtered
+			}
+
+			return state_new;
+		} else {
+			/* TODO: add error handling */
 		}
-
-		return state_new;
-	} else {
-		/* TODO: add error handling */
 	}
 }
 
 function renew_scopes(state = initialState, action) {
 	const message = action.message;
-
-	console.log('renew_scopes', action, message);
 
 	if (action.current_project_uuid !== message.project_uuid) { return state; }
 	else {
@@ -102,53 +101,59 @@ function update_comment(state = initialState, action) {
 	const _id = action.message['_id'];
 	const new_comment = action.message['comment'];
 
-	var new_state = JSON.parse(JSON.stringify(state));
-	for (var scope of new_state['ips']) {
-		if (scope._id == _id) {
-			scope.comment = new_comment;
+	if (action.current_project_uuid !== message.project_uuid) { return state; }
+	else {
+		var new_state = JSON.parse(JSON.stringify(state));
+		for (var scope of new_state['ips']) {
+			if (scope._id == _id) {
+				scope.comment = new_comment;
 
-			return new_state;
+				return new_state;
+			}
 		}
-	}
-	for (var scope of new_state['hosts']) {
-		if (scope._id == _id) {
-			scope.comment = new_comment;
+		for (var scope of new_state['hosts']) {
+			if (scope._id == _id) {
+				scope.comment = new_comment;
 
-			return new_state;
+				return new_state;
+			}
 		}
-	}
 
-	return state;
+		return state;
+	}
 }
 
 function update_scope(state = initialState, action) {
 	const message = action.message;
 
-	if (message["status"] == "success") {
-		var new_state = JSON.parse(JSON.stringify(state));
-		var updated_scope = message["updated_scope"];
+	if (action.current_project_uuid !== message.project_uuid) { return state; }
+	else {
+		if (message["status"] == "success") {
+			var new_state = JSON.parse(JSON.stringify(state));
+			var updated_scope = message["updated_scope"];
 
-		if (updated_scope["type"] == "ip") {
-			for (var ip_addr of new_state["ips"]) {
-				if (ip_addr["_id"] == updated_scope["_id"]) {
-					ip_addr["comment"] = updated_scope["comment"]
-					break;
+			if (updated_scope["type"] == "ip") {
+				for (var ip_addr of new_state["ips"]) {
+					if (ip_addr["_id"] == updated_scope["_id"]) {
+						ip_addr["comment"] = updated_scope["comment"]
+						break;
+					}
 				}
 			}
-		}
-		else if (updated_scope["type"] == "host") {
-			for (var host of new_state["hosts"]) {
-				if (host["_id"] == updated_scope["_id"]) {
-					host["comment"] = updated_scope["comment"]
-					break;
+			else if (updated_scope["type"] == "host") {
+				for (var host of new_state["hosts"]) {
+					if (host["_id"] == updated_scope["_id"]) {
+						host["comment"] = updated_scope["comment"]
+						break;
+					}
 				}
 			}
-		}
 
-		return new_state;
-	} else {
-		console.error(message);
-		/* TODO: add error handling */
+			return new_state;
+		} else {
+			console.error(message);
+			/* TODO: add error handling */
+		}
 	}
 
 }
