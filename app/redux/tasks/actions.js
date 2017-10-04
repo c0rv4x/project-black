18ff5ebb6cb3b919db50bcecbaacfef652e0bc9f
iad <bmth@me.com>
2017-10-04 13:59:09 +0300

    Fixed syntax bug

--- app/redux/tasks/actions.js
+++ app/redux/tasks/actions.js
@@ -6,28 +6,28 @@ export const UPDATE_TASKS = 'UPDATE_TASKS'
 
 export function newTask(message, current_project_uuid) {
 	return { type: NEW_TASK,
-		current_project_uuid: current_project_uuid
+		current_project_uuid: current_project_uuid,
 		message
 	}
 }
 
 export function changeStatusTask(message, current_project_uuid) {
 	return { type: CHANGE_STATUS_TASK,
-		current_project_uuid: current_project_uuid
+		current_project_uuid: current_project_uuid,
 		message
 	}
 }
 
 export function renewTasks(message, current_project_uuid) {
 	return { type: RENEW_TASKS,
-		current_project_uuid: current_project_uuid
+		current_project_uuid: current_project_uuid,
 		message
 	}
 }
 
 export function updateTasks(message, current_project_uuid) {
 	return { type: UPDATE_TASKS,
-		current_project_uuid: current_project_uuid
+		current_project_uuid: current_project_uuid,
 		message
 	}
 }
