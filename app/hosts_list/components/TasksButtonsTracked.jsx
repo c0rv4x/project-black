a6c819f762bff9c583b2bac3426aa66e12ff8331
iad <bmth@me.com>
2017-10-04 14:57:13 +0300

    Added options for dirsearching all ip addresses for each host or dirsearching single ip for each host

--- app/hosts_list/components/TasksButtonsTracked.jsx
+++ app/hosts_list/components/TasksButtonsTracked.jsx
@@ -25,17 +25,30 @@ class TasksButtonsTracked extends React.Component {
 
 			for (var each_port of [...ports]) {
 				let target = each_host.hostname;
-				// this.tasksEmitter.requestCreateTask('dirsearch', 
-				// 									[target + ":" + each_port], 
-				// 									{'program': options}, 
-				// 									this.props.project.project_uuid)
+				this.tasksEmitter.requestCreateTask('dirsearch', 
+													[target + ":" + each_port], 
+													{'program': options}, 
+													this.props.project.project_uuid);
 
-				for (var ip_address of each_host.ip_addresses) {
-					console.log(ip_address);
-					// this.tasksEmitter.requestCreateTask('dirsearch', 
-					// 									[target + ":" + each_port], 
-					// 									{'program': options}, 
-					// 									this.props.project.project_uuid)
+				if (options.dirsearch_all_ips) {
+					for (var ip_address of each_host.ip_addresses) {
+						let target = ip_address.ip_address;
+						this.tasksEmitter.requestCreateTask('dirsearch', 
+															[target + ":" + each_port], 
+															{'program': options}, 
+															this.props.project.project_uuid);
+					}
+				}
+				else if (options.dirsearch_single_ip) {
+					for (var ip_address of each_host.ip_addresses) {
+						let target = ip_address.ip_address;
+						this.tasksEmitter.requestCreateTask('dirsearch', 
+															[target + ":" + each_port], 
+															{'program': options}, 
+															this.props.project.project_uuid);
+
+						break;
+					}
 				}
 			}
 		}
@@ -91,7 +104,17 @@ class TasksButtonsTracked extends React.Component {
 								"name": "recursive",
 								"type": "checkbox",
 								"default_value": false
-							}
+							},
+							{
+								"name": "dirsearch_all_ips",
+								"type": "checkbox",
+								"default_value": false
+							},
+							{
+								"name": "dirsearch_single_ip",
+								"type": "checkbox",
+								"default_value": false
+							}					
 						]
 					}
 				]
