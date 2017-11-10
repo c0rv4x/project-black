fc1eb8afb7d8712f173b7b3c7e9fd9f36ae552d5
iad <bmth@me.com>
2017-11-10 18:07:49 +0300

    Added hiding of scope setup tables

--- app/scope_setup/presentational/IPTable.jsx
+++ app/scope_setup/presentational/IPTable.jsx
@@ -1,12 +1,22 @@
 import _ from 'lodash'
 import React from 'react'
 
-import { Table, Button } from 'semantic-ui-react'
+import { Table, Button, Label, Transition } from 'semantic-ui-react'
 
 
 class IPTable extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
+
+		this.state = {
+			visible: false
+		}
+
+		this.toggleVisibility.bind(this);
+	}
+
+	toggleVisibility() {
+		this.setState({ visible: !this.state.visible });
 	}
 
 	render() {
@@ -19,18 +29,27 @@ class IPTable extends React.Component {
 			)
 		});
 
+		let visible = this.state.visible;
+
 		return (
-			<Table>
-				<Table.Header>
-					<Table.Row>
-						<Table.HeaderCell>IP address</Table.HeaderCell>
-						<Table.HeaderCell>Control</Table.HeaderCell>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{ips_rendered}
-				</Table.Body>
-			</Table>			
+			<span>
+				<Label onClick={this.toggleVisibility.bind(this)} size="large" color="blue">
+					{ips_rendered.length} ips
+				</Label>
+				<Transition visible={visible} animation='scale' duration={500}>
+					<Table>
+						<Table.Header>
+							<Table.Row>
+								<Table.HeaderCell>IP address</Table.HeaderCell>
+								<Table.HeaderCell>Control</Table.HeaderCell>
+							</Table.Row>
+						</Table.Header>
+						<Table.Body>
+							{ips_rendered}
+						</Table.Body>
+					</Table>
+				</Transition>
+			</span>
 		)
 	}
 }
