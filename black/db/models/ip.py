e07f45b996722d22166ef8f1481eb058a3d388a2
iad <bmth@me.com>
2018-02-16 14:22:49 +0300

    IPDatabase is not loading hosts as 'noload'

--- black/db/models/ip.py
+++ black/db/models/ip.py
@@ -44,7 +44,6 @@ class IPDatabase(Scope):
         "HostDatabase",
         secondary=association_table,
         back_populates="ip_addresses",
-        # lazy='joined'
         lazy='noload'
     )
 
