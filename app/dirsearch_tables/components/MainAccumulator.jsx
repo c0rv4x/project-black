7e409a2d7ea819f151dee14ab66b1c17e6459643
iad <bmth@me.com>
2018-02-27 10:43:09 +0300

    Added perf for dirsearch table

--- app/dirsearch_tables/components/MainAccumulator.jsx
+++ app/dirsearch_tables/components/MainAccumulator.jsx
@@ -4,11 +4,29 @@ import { Header } from 'semantic-ui-react'
 
 import MainTable from './MainTable.jsx'
 
+import Perf from 'react-addons-perf'
 
 class MainAccumulator extends React.Component {
 	constructor(props) {
 		super(props);
 	}
+
+componentDidMount() {
+    // We have to push it to the next tick otherwise React Native would have
+    // problems with the WorkerPerformance
+    setTimeout(() => {
+      Perf.start();
+
+      // Some arbitrary time for metrics
+      setTimeout(() => {
+        Perf.stop();
+        Perf.printInclusive();
+        Perf.printExclusive();
+      }, 25000);
+
+    }, 0);
+}	
+
 
 	render() {
 		const { ips, hosts, project } = this.props;
