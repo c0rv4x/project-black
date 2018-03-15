c1784afce66651b7c8e8d912d83461cd26a5b505
iad <bmth@me.com>
2018-03-15 16:09:37 +0300

    Ips list now has a button for starting regular nmap

--- app/ips_list/components/TitleButtonsWithHandlers.jsx
+++ app/ips_list/components/TitleButtonsWithHandlers.jsx
@@ -92,20 +92,20 @@ class TitleButtonsWithHandlers extends React.Component {
 										}
 									]
 						  		},
-						  		// {
-						  	// 		'name': 'Nmap',
-						  	// 		'handler': this.runNmap,
-									// "preformed_options": [
-									// 	{
-									// 		"name": "All Ports",
-									// 		"options": "-p1-65535"
-									// 	},
-									// 	{
-									// 		"name": "Top ports",
-									// 		"options": "-p80,23,443,21,22,25,3389,110,445,139,143,53,135,3306,8080,1723,111,995,993,5900,1025,587,8888,199,1720,113,554,256"
-									// 	}
-									// ]
-						  	// 	},
+						  		{
+						  			'name': 'Nmap',
+						  			'handler': this.runNmap,
+									"preformed_options": [
+										{
+											"name": "All Ports",
+											"options": "-p1-65535"
+										},
+										{
+											"name": "Top ports",
+											"options": "--top-ports=2500"
+										}
+									]
+						  		},
 						  		{
 						  			"name": "Nmap Only Open Ports",
 						  			"handler": this.runNmapOnlyOpen,
