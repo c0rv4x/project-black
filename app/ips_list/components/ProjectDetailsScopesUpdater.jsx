766d532d985d8cf0d71ca44fdd74233bc403747a
iad <bmth@me.com>
2018-03-19 20:50:22 +0300

    Fixed filtering of tables. When filter was applied on the initial page, some data was lost

--- app/ips_list/components/ProjectDetailsScopesUpdater.jsx
+++ app/ips_list/components/ProjectDetailsScopesUpdater.jsx
@@ -38,8 +38,11 @@ class ProjectDetailsScopesUpdater extends React.Component {
 	componentWillReceiveProps(nextProps) {
 		var { ips, filters } = nextProps;
 
-		if ((ips.update_needed === true) || (!_.isEqual(filters, this.props.filters))) {
+		if (ips.update_needed === true) {
 			this.renewIps(nextProps.ips.page, filters);
+		}
+		else if (!_.isEqual(filters, this.props.filters)) {
+			this.renewIps(0, filters);
 		}
 
 		if (this.state.loading) {
