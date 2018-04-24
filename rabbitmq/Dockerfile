e40dcc47771530397ee9b31efb93adc0904dc8fa
Anatoly Ivanov <bmth@me.com>
2018-04-24 22:02:12 +0300

    Added docker compose to make the deployment faster

---
+++ rabbitmq/Dockerfile
@@ -0,0 +1,4 @@
+FROM rabbitmq:3-management
+RUN apt-get update
+RUN apt-get install -y curl 
+EXPOSE 5672 15672
--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,8 @@
-docker start psql
-docker start rabbit
-tmux new -d -s black "source .env/bin/activate && python3 app.py"
-tmux new-window -t "black:01" -n "masscan" "cd black/ && source .env/bin/activate && sudo python3 masscan_worker.py"
-tmux new-window -t "black:02" -n "nmap" "cd black/ && source .env/bin/activate && python3 nmap_launcher.py"
-tmux new-window -t "black:03" -n "dirsearch" "cd black/ && source .env/bin/activate && python3 dirsearch_worker.py"
+tmux new -d -s black "python3 app.py"
+tmux new-window -t "black:01" -n "masscan" "cd black/ && sudo python3 masscan_worker.py"
+tmux new-window -t "black:02" -n "nmap" "cd black/ && python3 nmap_launcher.py"
+tmux new-window -t "black:03" -n "dirsearch" "cd black/ && python3 dirsearch_worker.py"
+
+tmux ls
+
+while true; do sleep 1000; done
