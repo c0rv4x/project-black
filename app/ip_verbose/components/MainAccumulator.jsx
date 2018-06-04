2b5e945f3b07cd1176a77f269a6b71d253a61fc2
Anatoly Ivanov <bmth@me.com>
2018-06-04 13:02:01 +0300

    Fixed comment on ip verbose page

--- app/ip_verbose/components/MainAccumulator.jsx
+++ app/ip_verbose/components/MainAccumulator.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import IPsSocketioEventsEmitter from '../../redux/ips/IPsSocketioEventsEmitter.js'
 import ScopeComment from '../../common/scope_comment/ScopeComment.jsx'
 import PortsTabs from '../../host_verbose/presentational/PortsTabs.jsx'
 
@@ -11,9 +12,16 @@ class MainAccumulator extends React.Component {
 		this.state = {
 			'activeTabNumber': null,
 			'activePortNumber': null
-		}				
+		}		
+
+		this.ipsEmitter = new IPsSocketioEventsEmitter();		
+
+		if (this.props.update_needed) {
+			this.ipsEmitter.requestSingleIPs(this.props.project_uuid, this.props.ip.ip_address);
+		}
 
 		this.tabChange = this.tabChange.bind(this);
+		this.commentSubmitted = this.commentSubmitted.bind(this);
 	}
 
 	componentWillReceiveProps(newProps) {
@@ -25,6 +33,10 @@ class MainAccumulator extends React.Component {
 				});
 			}
 		}
+
+		if (newProps.update_needed) {
+			this.ipsEmitter.requestSingleIPs(this.props.project_uuid, this.props.ip.ip_address);
+		}		
 	}
 
 	tabChange(newNumber, portNumber) {
@@ -34,13 +46,19 @@ class MainAccumulator extends React.Component {
 		});
 	}
 
+	commentSubmitted(comment, _id) {
+		this.ipsEmitter.requestUpdateIP(comment, _id, this.props.project_uuid, 'ip_address');
+	}
+
 	render() {
 		let { ip, ports, files } = this.props;
 
 		return (
 			<div>
 				<h2>{ip.ip_address}</h2>
-				<ScopeComment commentValue={ip.comment} />
+				<ScopeComment
+					comment={ip.comment}
+					onCommentSubmit={(value) => this.commentSubmitted(value, ip.ip_id)} />
 
 				<PortsTabs ports={ports}
 					   	   activeTabNumber={this.state.activeTabNumber}
