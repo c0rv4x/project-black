714d41856d668550c95f2a2cb6c5fb57cf30d747
Anatoly Ivanov <bmth@me.com>
2018-07-04 14:50:02 +0300

    Removed old docker scripts

--- docker_setup.sh
+++
@@ -1,4 +0,0 @@
-docker pull postgres
-docker run -d -p 127.0.0.1:5432:5432 --name psql postgres
-docker pull rabbitmq
-docker run -d -p 127.0.0.1:5672:5672 --name rabbit rabbitmq
--- start_all.sh
+++
@@ -1,8 +0,0 @@
-tmux new -d -s black "python3 app.py"
-tmux new-window -t "black:01" -n "masscan" "cd black/ && sudo python3 masscan_worker.py"
-tmux new-window -t "black:02" -n "nmap" "cd black/ && python3 nmap_launcher.py"
-tmux new-window -t "black:03" -n "dirsearch" "cd black/ && python3 dirsearch_worker.py"
-
-tmux ls
-
-while true; do sleep 1000; done
