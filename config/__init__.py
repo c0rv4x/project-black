c12a57f02025e1f03117af3ed11db34402e21e68
Anatoly Ivanov <bmth@me.com>
2018-07-08 14:38:42 +0300

    Remove black from git ignore

--- config/__init__.py
+++ config/__init__.py
@@ -5,7 +5,8 @@ import yaml
 CONFIG_FILE = "./config/config.yaml"
 
 if len(sys.argv) > 1:
-	CONFIG_FILE = "./config/{}".format(sys.argv[1])
+	if sys.argv[1] == "docker":
+		CONFIG_FILE = "./config/config_docker.yaml"
 
 with open(CONFIG_FILE) as w:
     CONFIG = yaml.load(w.read())
