e06505fd22f47acb9574e36f8fe2bca7cce22282
Anatoly Ivanov <bmth@me.com>
2018-08-07 18:58:32 +0300

    Added exception catcher for dirsearch poll_status

--- black/workers/dirsearch/dirsearch_task.py
+++ black/workers/dirsearch/dirsearch_task.py
@@ -66,17 +66,20 @@ class DirsearchTask(AsyncTask):
         data = await reader.read(1000)
 
         if data:
-            decoded_data = json.loads(data.decode('utf-8'))
-            status = decoded_data['status']
-            progress = decoded_data['progress']
+            try:
+                decoded_data = json.loads(data.decode('utf-8'))
+                status = decoded_data['status']
+                progress = decoded_data['progress']
 
-            if status == 'Finished' or status == 'Aborted':
-                await self.set_status(status, progress=progress, text=self.target)
+                if status == 'Finished' or status == 'Aborted':
+                    await self.set_status(status, progress=progress, text=self.target)
 
-                self.all_done.release()
-                return
-            else:
-                await self.set_status(status, progress=progress)
+                    self.all_done.release()
+                    return
+                else:
+                    await self.set_status(status, progress=progress)
+            except Exception as exc:
+                print("DirsearchTask:poll_status", exc, data)
 
         self.loop.create_task(self.poll_status(reader))
 
