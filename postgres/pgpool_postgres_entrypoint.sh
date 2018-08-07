bdcce88726e5d294dd8e0a707ab45fd9943d21e0
Anatoly Ivanov <bmth@me.com>
2018-08-07 14:40:22 +0300

    Added pgpool script start for postgres container

--- postgres/pgpool_postgres_entrypoint.sh
+++ postgres/pgpool_postgres_entrypoint.sh
@@ -1,2 +1,14 @@
 tmux new -d -s postgres "docker-entrypoint.sh postgres"
-while true; do pgpool -n; sleep 1; done
+
+RES=`pg_isready -U postgres | grep -o "accepting"`
+
+while [[ $RES != "accepting" ]];
+do 
+	RES=`pg_isready -U postgres | grep -o "accepting"`
+	sleep 1
+done
+
+rm /var/run/postgresql/.s.PGSQL.5433
+rm /var/run/postgresql/.s.PGSQL.9898
+
+pgpool -n
