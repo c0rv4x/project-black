f64c739757c9c97208102655d3b575fab2189fc5
Anatoly Ivanov <bmth@me.com>
2018-08-07 14:40:56 +0300

    Each worker is run in a separate docker container

--- start_all.sh
+++
@@ -1,8 +0,0 @@
-tmux new -d -s black "python3 app.py docker"
-tmux new-window -t "black:01" -n "masscan" "sudo python3 masscan_worker.py docker"
-tmux new-window -t "black:02" -n "nmap" "python3 nmap_worker.py docker"
-tmux new-window -t "black:03" -n "dirsearch" "python3 dirsearch_worker.py docker"
-
-tmux ls
-
-while true; do sleep 1000; done
