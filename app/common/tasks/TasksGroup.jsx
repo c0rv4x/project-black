3013260730dbccd51740642fa5fbdaf8b29a548b
Anatoly Ivanov <bmth@me.com>
2018-09-05 18:47:21 +0300

    Tasks table now has progress bars of separate tasks instead of previous numbers

---
+++ app/common/tasks/TasksGroup.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+	Label,
+	Progress,
+	Table
+} from 'semantic-ui-react'
+
+
+class EachTask extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		let { tasks, type } = this.props;
+		let progresses = [];
+
+		for (let each_task of tasks) {
+			console.log(each_task);
+			if (each_task.status == 'Working') {
+				progresses.push(
+					<Progress
+						id={"progress_task_" + each_task.task_id}
+						key={"progress_task_" + each_task.task_id}
+						percent={each_task.progress}
+						size="medium"
+						progress
+					/>
+				);
+			}
+		}
+
+
+		return (
+			<Table.Row>
+				<Table.Cell>{type}</Table.Cell>
+				<Table.Cell>{tasks.length}</Table.Cell>
+				<Table.Cell>{progresses}</Table.Cell>
+			</Table.Row>
+		)
+	}
+}
+
+export default EachTask;
