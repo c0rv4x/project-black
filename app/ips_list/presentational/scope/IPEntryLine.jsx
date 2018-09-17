f60a07579894a90fb4dd29027b7afe28f0baf24b
Anatoly Ivanov <bmth@me.com>
2018-09-18 01:03:46 +0300

    Table with creds is now sortable

--- app/ips_list/presentational/scope/IPEntryLine.jsx
+++ app/ips_list/presentational/scope/IPEntryLine.jsx
@@ -22,6 +22,43 @@ class IPEntryLine extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			column: null,
+			data: [],
+			direction: null
+		};
+
+		this.handleSortClick = this.handleSortClick.bind(this);
+	}
+
+	componentDidUpdate(prevProps) {
+		if (prevProps.ip.creds !== this.props.ip.creds) {
+			this.setState({
+				column: null,
+				data: this.props.ip.creds,
+				direction: null
+			});
+		}
+	}
+
+	handleSortClick(clickedColumn) {
+		const { column, data, direction } = this.state
+
+		if (column !== clickedColumn) {
+			this.setState({
+				column: clickedColumn,
+				data: _.sortBy(data, [clickedColumn]),
+				direction: 'ascending',
+			});
+	  
+			return
+		}
+	  
+		this.setState({
+			data: data.reverse(),
+			direction: direction === 'ascending' ? 'descending' : 'ascending',
+		})
 	}
 
 	render() {
@@ -86,33 +123,72 @@ class IPEntryLine extends React.Component {
 		}
 
 		let creds_rendered = null;
+		if (this.state.data.length) {
+			const { column, data, direction } = this.state;
 
-		if (ip.creds.length) {
 			creds_rendered = (
 				<Modal 
 					trigger={<Label as="a">Accounts<Label.Detail>{ip.creds.length}</Label.Detail></Label>}
 				>
 					<Modal.Header>{ip.ip_address}</Modal.Header>
 					<Modal.Content>
-						<Table>
+						<Table sortable>
 							<Table.Header>
 								<Table.Row>
-									<Table.HeaderCell width={1}>Port</Table.HeaderCell>
-									<Table.HeaderCell width={1}>Code</Table.HeaderCell>
-									<Table.HeaderCell width={5}>Candidate</Table.HeaderCell>
-									<Table.HeaderCell width={1}>Size</Table.HeaderCell>
-									<Table.HeaderCell width={6}>Message</Table.HeaderCell>
+									<Table.HeaderCell
+										sorted={column === 'port_number' ? direction : null}
+										onClick={() => {this.handleSortClick('port_number')}}
+										width={1}
+									>
+										Port
+									</Table.HeaderCell>
+									<Table.HeaderCell
+										sorted={column === 'service' ? direction : null}
+										onClick={() => {this.handleSortClick('service')}}
+										width={2}
+									>
+										Service
+									</Table.HeaderCell>
+									<Table.HeaderCell
+										sorted={column === 'code' ? direction : null}
+										onClick={() => {this.handleSortClick('code')}}
+										width={1}
+									>
+										Code
+									</Table.HeaderCell>
+									<Table.HeaderCell
+										sorted={column === 'candidate' ? direction : null}
+										onClick={() => {this.handleSortClick('candidate')}}
+										width={5}
+									>
+										Candidate
+									</Table.HeaderCell>
+									<Table.HeaderCell
+										sorted={column === 'size' ? direction : null}
+										onClick={() => {this.handleSortClick('size')}}
+										width={1}
+									>
+										Size
+									</Table.HeaderCell>
+									<Table.HeaderCell
+										sorted={column === 'mesg' ? direction : null}
+										onClick={() => {this.handleSortClick('mesg')}}
+										width={6}
+									>
+										Message
+									</Table.HeaderCell>
 								</Table.Row>
 							</Table.Header>
 							<Table.Body>
-								{ip.creds.map((cred) => {
+								{_.map(data, ({ id, target, port_number, service, code, candidate, size, mesg}) => {
 									return (
-										<Table.Row key={"cred_" + cred.target + "_" + cred.id}>
-											<Table.Cell>{cred.port_number}</Table.Cell>
-											<Table.Cell>{cred.code}</Table.Cell>
-											<Table.Cell>{cred.candidate}</Table.Cell>
-											<Table.Cell>{cred.size}</Table.Cell>
-											<Table.Cell>{cred.mesg}</Table.Cell>
+										<Table.Row key={"cred_" + target + "_" + id}>
+											<Table.Cell>{port_number}</Table.Cell>
+											<Table.Cell>{service}</Table.Cell>
+											<Table.Cell>{code}</Table.Cell>
+											<Table.Cell>{candidate}</Table.Cell>
+											<Table.Cell>{size}</Table.Cell>
+											<Table.Cell>{mesg}</Table.Cell>
 										</Table.Row>
 									);
 								})}
