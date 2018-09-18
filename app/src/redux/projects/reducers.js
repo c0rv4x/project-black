43335c546d9094eb47dd9a6c6aa4522fea1cbbfa
Anatoly Ivanov <bmth@me.com>
2018-09-18 20:59:26 +0300

    Removed status checker from projects reducers

--- app/src/redux/projects/reducers.js
+++ app/src/redux/projects/reducers.js
@@ -16,73 +16,57 @@ const initialState = {
 function create_project(state = [], action) {
 	const message = action.message;
 
-	if (message["status"] == 'success') {
-		var state_new = state.slice();
+	var state_new = state.slice();
 
-		state_new.push({
-			"project_name": message["new_project"]["project_name"],
-			"project_uuid": message["new_project"]["project_uuid"],
-			"comment": message["new_project"]["comment"]
-		});
+	state_new.push({
+		"project_name": message["new_project"]["project_name"],
+		"project_uuid": message["new_project"]["project_uuid"],
+		"comment": message["new_project"]["comment"]
+	});
 
-		return state_new;
-	} else {
-		/* TODO: add error handling */
-	}
+	return state_new;
 }
 
 function delete_project(state = [], action) {
 	const message = action.message;
 
-	if (message["status"] == 'success') {
-		var state_new = state.slice();
+	var state_new = state.slice();
 
-		var projects_filtered = _.filter(state_new, (x) => {
-			return x["project_uuid"] != message["project_uuid"];
-		});
-		state_new = projects_filtered;
+	var projects_filtered = _.filter(state_new, (x) => {
+		return x["project_uuid"] != message["project_uuid"];
+	});
+	state_new = projects_filtered;
 
-		return state_new;
-	} else {
-		/* TODO: add error handling */
-	}
+	return state_new;
 }
 
 function renew_projects(state = [], action) {
 	const message = action.message;
 
-	if (message["status"] == 'success') {
-		var state_new = message['projects'];
+	var state_new = message['projects'];
 
-		return state_new;
-	} else {
-		/* TODO: add error handling */
-	}		
+	return state_new;
 }
 
 function update_project(state = [], action) {
 	const message = action.message;
 
-	if (message["status"] == 'success') {
-		var new_project = message["new_project"];
-		var project_name = null;
+	var new_project = message["new_project"];
+	var project_name = null;
 
-		var state_new = _.filter(state, (x) => {
-			var matches_filter = x.project_uuid != new_project.project_uuid;
+	var state_new = _.filter(state, (x) => {
+		var matches_filter = x.project_uuid != new_project.project_uuid;
 
-			if (!matches_filter) project_name = x.project_name;
+		if (!matches_filter) project_name = x.project_name;
 
-			return matches_filter;
-		});
+		return matches_filter;
+	});
 
-		if (new_project.project_name === null) new_project.project_name = project_name;
+	if (new_project.project_name === null) new_project.project_name = project_name;
 
-		state_new.push(new_project);
+	state_new.push(new_project);
 
-		return state_new;
-	} else {
-		/* TODO: add error handling */
-	}		
+	return state_new;
 }
 
 function update_comment(state = [], action) {
