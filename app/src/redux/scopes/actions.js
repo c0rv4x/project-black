3a7904613c1f40efd0503ae1d9c445ee03618f98
Anatoly Ivanov <bmth@me.com>
2018-09-18 21:01:36 +0300

    Removed some old code from scope's redux

--- app/src/redux/scopes/actions.js
+++ app/src/redux/scopes/actions.js
@@ -1,21 +1,9 @@
 export const CREATE_SCOPE = 'CREATE_SCOPE'
-export const DELETE_SCOPE = 'DELETE_SCOPE'
-export const RENEW_SCOPES = 'RENEW_SCOPES'
-export const UPDATE_SCOPE = 'UPDATE_SCOPE'
-export const RESOLVE_SCOPES = 'RESOLVE_SCOPES'
 
 
 export function createScope(message, current_project_uuid) {
 	return {
 		type: CREATE_SCOPE,
-		current_project_uuid: current_project_uuid,
-		message
-	}
-}
-
-export function deleteScope(message, current_project_uuid) {
-	return {
-		type: DELETE_SCOPE,
 		current_project_uuid: current_project_uuid,
 		message
 	}
