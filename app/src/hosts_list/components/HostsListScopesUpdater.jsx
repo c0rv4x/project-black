58d7f3ab8313083a26c93bd8c200e2a1da0721c3
Anatoly Ivanov <bmth@me.com>
2018-10-04 17:28:10 +0300

    Added action for host's creds delete button
    Although there is no "hosts should update" reaction from the server yet

--- app/src/hosts_list/components/HostsListScopesUpdater.jsx
+++ app/src/hosts_list/components/HostsListScopesUpdater.jsx
@@ -4,6 +4,7 @@ import { Dimmer, Loader, Segment } from 'semantic-ui-react'
 
 import HostsList from './HostsList.jsx'
 import HostsSocketioEventsEmitter from '../../redux/hosts/HostsSocketioEventsEmitter.js'
+import CredsSocketioEventsEmitter from '../../redux/creds/CredsSocketioEventsEmitter.js'
 import { setLoaded } from '../../redux/hosts/actions.js'
 
 
@@ -12,15 +13,19 @@ class HostsListScopesUpdater extends React.Component {
 		super(props);
 
 		this.hostsEmitter = new HostsSocketioEventsEmitter();
+		this.credsEmitter = new CredsSocketioEventsEmitter();
 		this.triggerSetLoaded = this.triggerSetLoaded.bind(this);
 
-		var { hosts } = this.props;
-
-		if (hosts.update_needed === true) {
-			this.renewHosts(hosts.page, hosts.page_size);
+		if (this.props.hosts.update_needed === true) {
+			this.renewHosts();
+		}
+		else {
+			// console.log("Constructor renewing creds");
+			this.renewCreds();
 		}
 
 		this.renewHosts = this.renewHosts.bind(this);
+		this.renewCreds = this.renewCreds.bind(this);
 		this.requestUpdateHost = this.requestUpdateHost.bind(this);
 	}
 
@@ -40,6 +45,10 @@ class HostsListScopesUpdater extends React.Component {
 		this.hostsEmitter.requestRenewHosts(this.props.project_uuid, filters, page, this.props.hosts.page_size);
 	}
 
+	renewCreds(hosts=this.props.hosts.data) {
+		this.credsEmitter.renewCreds(this.props.project_uuid, hosts.map((host) => {return host.hostname}));
+	}
+
 	requestUpdateHost(comment, _id) {
 		this.hostsEmitter.requestUpdateHost(comment, _id, this.props.project_uuid, "host");
 	}
@@ -51,9 +60,15 @@ class HostsListScopesUpdater extends React.Component {
 			this.triggerSetLoaded(false);
 			this.renewHosts(hosts.page, hosts.page_size, filters);
 		}
-		else if (!_.isEqual(filters, prevProps.filters)) {
-			this.triggerSetLoaded(false);
-			this.renewHosts(0, hosts.page_size, filters);
+		else {
+			if ((prevProps.hosts.update_needed === true) || (!_.isEqual(hosts.data, prevProps.hosts.data))) {
+				// console.log("componentdidupdate is renewing creds");
+				this.renewCreds();
+			}
+			if (!_.isEqual(filters, prevProps.filters)) {
+				this.triggerSetLoaded(false);
+				this.renewHosts(0, hosts.page_size, filters);
+			}
 		}
 	}
 
