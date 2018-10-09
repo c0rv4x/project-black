506e588b004732045bd571e1b5533047dc67f6e7
Anatoly Ivanov <bmth@me.com>
2018-10-09 19:29:37 +0300

    Added skeleton table for dictionaries

--- app/src/common/tasks_buttons/components/DictionariesManager.jsx
+++ app/src/common/tasks_buttons/components/DictionariesManager.jsx
@@ -1,42 +1,23 @@
 import _ from 'lodash'
 import React from 'react'
 import {
-    Button,
-	Divider,
-    Icon
+    Divider,
+    Header
 } from 'semantic-ui-react'
 
-import DictionaryInput from './DictionaryInput.jsx'
+import DictionaryStats from './DictionaryStats.jsx'
 
 
 class DictionariesManager extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            inputFields: []
-        };
-
-        this.addNewField = this.addNewField.bind(this);
-    }
-
-    addNewField() {
-        const { inputFields } = this.state;
-
-        this.setState({
-            inputFields: [
-                ...inputFields,
-                <DictionaryInput key={inputFields.length} name={inputFields.length} />
-            ]
-        });
     }
 
     render() {
 		return (
 			<div>
-                {this.state.inputFields}
-
-                <Button size="tiny" onClick={this.addNewField}><Icon name="plus" />Add new field</Button>
+                <Header as="h3">Dictionaries</Header>
+                <DictionaryStats />
                 <Divider hidden />
             </div>
 		)
---
+++ app/src/common/tasks_buttons/components/DictionaryStats.jsx
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+import React from 'react'
+import {
+    Table
+} from 'semantic-ui-react'
+
+
+class DictionariesStats extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+		return (
+			<Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Header</Table.HeaderCell>
+                        <Table.HeaderCell>Header</Table.HeaderCell>
+                        <Table.HeaderCell>Header</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+            </Table>
+		)
+	}
+
+}
+
+export default DictionariesStats;
+
+
+
+
