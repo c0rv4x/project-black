64405a70e8ef80ba82e9803df4d410a148cf62a8
Anatoly Ivanov <bmth@me.com>
2019-01-08 21:13:52 +0300

    Loading is more beautiful

--- app/src/common/loading/Loading.jsx
+++ app/src/common/loading/Loading.jsx
@@ -12,7 +12,7 @@ const spinning = (
     >
       <path
         style={{fill: 'white'}}
-        opacity=".25"
+        opacity=".80"
         d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"
       />
       <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z">
@@ -29,7 +29,7 @@ const spinning = (
   );
   
 const loading = (
-    <Box background="grey" align="center" justify="center" style={{ height: '100px', opacity: 1 }}>
+    <Box  align="center" justify="center" style={{ height: '100px', opacity: 1 }}>
         {spinning}
     </Box>
 );
@@ -42,14 +42,12 @@ class Loading extends React.Component {
         >
             {this.props.children}
             {
-                !this.props.componentLoading && (
+                this.props.componentLoading && (
                     <Layer
                         modal
                         animation={false}
                     >
-                        <Box direction="row" align="center">
-                            {loading}
-                        </Box>
+                        {spinning}
                     </Layer>
                 )
             }
