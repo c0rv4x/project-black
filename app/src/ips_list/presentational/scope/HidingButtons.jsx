ce587b889a63967f9d4d7a435e6920825e856fe2
Anatoly Ivanov <bmth@me.com>
2019-01-11 04:53:22 +0300

    Button for scopes now do the actions

--- app/src/ips_list/presentational/scope/HidingButtons.jsx
+++ app/src/ips_list/presentational/scope/HidingButtons.jsx
@@ -12,43 +12,20 @@ import { Google, Inspect } from 'grommet-icons'
 
 
 class HidingButtons extends React.Component {
-	constructor(props) {
-        super(props);
-        
-        this.state = {
-            "opened": false
-        }
-
-    }
-    
-    renderAllElement() {
+	render() {
         const { project_uuid, type, target } = this.props;
         const verbose_host_link = '/project/' + project_uuid + '/' + type + '/' + target;
 
-        return (
-                <Dropdown icon="ellipsis horizontal">
-                    <Dropdown.Menu>
-                        <Dropdown.Item
-                            icon="google"
-                            content="Google Dork"
-                            onClick={() => window.open("https://google.com/search?q=site:" + target, Math.random().toString(36).substring(7))}
-                        />
-                        <Dropdown.Item
-                            icon="cogs"
-                            content="Details"
-                            onClick={() => window.open(verbose_host_link, Math.random().toString(36).substring(7), 'width=850,height=700')}
-                        />
-                    </Dropdown.Menu>
-                </Dropdown>
-
-        )
-    }
-
-	render() {
 		return (
             <Box direction="row" align="center">
-                <Button icon={<Google size="medium" />} />
-                <Button icon={<Inspect size="medium" />} />
+                <Button
+                    onClick={() => window.open("https://google.com/search?q=site:" + target, Math.random().toString(36).substring(7))}
+                    icon={<Google size="medium" />}
+                />
+                <Button
+                    onClick={() => window.open(verbose_host_link, Math.random().toString(36).substring(7), 'width=850,height=700')}
+                    icon={<Inspect size="medium" />}
+                />
             </Box>
 		)	
 	}
