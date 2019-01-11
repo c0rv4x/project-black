ca3347864e4adfc9d5d26189841baafc43ee020a
Anatoly Ivanov <bmth@me.com>
2019-01-11 05:43:33 +0300

    Changed hosts table to use grommet

--- app/src/hosts_list/components/HostsTable.jsx
+++ app/src/hosts_list/components/HostsTable.jsx
@@ -5,18 +5,34 @@ import Search from '../../common/search/Search.jsx'
 import ReactPaginate from '../../common/paginate/ReactPaginate.jsx'
 import HostsEntryLine from '../presentational/HostsEntryLine.jsx'
 
-import { Grid, Container, Header } from 'semantic-ui-react'
+import { Grid, Heading } from 'grommet'
 
 class HostsTable extends React.Component {
 
 	constructor(props) {
 		super(props);
 
-		if (this.props.hosts) {
+		const { hosts } = this.props;
+
+		if (hosts) {
+			let rows = [];
+			let areas = [];
+	
+			for (let i = 0; i < hosts.data.length; i++) {
+				const host = hosts.data[i];
+	
+				rows.push('auto');
+				areas.push({
+					name: 'host_' + host.host_id, start: [0, i], end: [0, i]
+				});
+			}
+
 			this.state = {
-				shownData: this.props.hosts.data,
-				offsetPage: this.props.hosts.page,
-				pageCount: Math.ceil(this.props.hosts.total_db_hosts / this.props.hosts.page_size)
+				shownData: hosts.data,
+				offsetPage: hosts.page,
+				pageCount: Math.ceil(hosts.total_db_hosts / hosts.page_size),
+				areas: areas,
+				rows: rows
 			}
 		}
 
@@ -24,22 +40,44 @@ class HostsTable extends React.Component {
 		this.handlePageClick = this.handlePageClick.bind(this);
 	}
 
-	componentWillReceiveProps(nextProps) {
-		if ((nextProps.hosts.page !== this.props.hosts.page) || (nextProps.hosts.page_size !== this.props.hosts.page_size)) {
+	shouldComponentUpdate(nextProps, nextState) {
+		return (!_.isEqual(nextProps, this.props) || !_.isEqual(this.state, nextState));
+	}
+
+	componentDidUpdate(prevProps) {
+		if ((this.props.hosts.page !== prevProps.hosts.page) || (this.props.hosts.page_size !== prevProps.hosts.page_size)) {
 			this.props.triggerSetLoaded(true);
 		}
 
-		if (nextProps.hosts.selected_hosts !== this.props.hosts.selected_hosts) {
+		const { hosts } = this.props;
+		let rows = [];
+		let areas = [];
+
+		for (let i = 0; i < hosts.data.length; i++) {
+			const host = hosts.data[i];
+
+			rows.push('auto');
+			areas.push({
+				name: 'host_' + host.host_id, start: [0, i], end: [0, i]
+			});
+		}
+
+		this.setState({
+			rows: rows,
+			areas: areas
+		});
+
+		if (this.props.hosts.selected_hosts !== prevProps.hosts.selected_hosts) {
 			this.setState({
-				shownData: nextProps.hosts.data,
+				shownData: this.props.hosts.data,
 				offsetPage: 0,
-				pageCount: Math.ceil(nextProps.hosts.selected_hosts / this.props.hosts.page_size)
+				pageCount: Math.ceil(this.props.hosts.selected_hosts / this.props.hosts.page_size)
 			});
 		}
 		else {
 			this.setState({
-				shownData: nextProps.hosts.data,
-				pageCount: Math.ceil(nextProps.hosts.selected_hosts / this.props.hosts.page_size)
+				shownData: this.props.hosts.data,
+				pageCount: Math.ceil(this.props.hosts.selected_hosts / this.props.hosts.page_size)
 			});
 		}
 	}
@@ -50,66 +88,48 @@ class HostsTable extends React.Component {
 		this.props.renewHosts(page_number - 1, this.props.hosts.page_size);
 	}
 
-	shouldComponentUpdate(nextProps, nextState) {
-		return (!_.isEqual(nextProps, this.props) || !_.isEqual(this.state, nextState));
-	}
-
 	commentSubmitted(comment, _id) {
 		this.props.requestUpdateHost(comment, _id);
 	}
 
 	render() {
-		const scopes = _.map(this.state.shownData, (x) => {
-			return <HostsEntryLine key={x.host_id}
-								   project_uuid={this.props.project_uuid}
-								   host={x} 
-								   onCommentSubmit={(value) => this.commentSubmitted(value, x.host_id)}
-								   deleteScope={() => this.props.deleteScope(x.host_id)} />
+		const { areas, rows } = this.state;
+
+		const hosts = _.map(this.state.shownData, (x) => {
+			return (
+				<HostsEntryLine
+					key={x.host_id} 
+					host={x}
+					project_uuid={this.props.project_uuid}
+					onCommentSubmit={(value) => this.commentSubmitted(value, x.host_id)}
+					deleteHost={() => this.props.deleteScope(x.host_id)}
+				/>
+			)
 		});
-
-		let hosts_group = [];
-
-		let columns = 3;
-		for (var i = 0; i < scopes.length + columns - 1; i += columns) {
-			let hosts_slice = scopes.slice(i, Math.min(scopes.length, i + columns));
-
-			let host_key_sum = '';
-			let hosts_slice_formatted = hosts_slice.map((host) => {
-				host_key_sum += host.key + ',';
-
-				return (
-					<Grid.Column key={host.key + '__'}>
-						{host}
-					</Grid.Column>
-				);
-			});
-
-			hosts_group = hosts_group.concat(
-				<Grid.Row stretched key={host_key_sum}>
-					{hosts_slice_formatted}
-				</Grid.Row>
-			);
-		}
 
 		return (
 			<div>
 				<Search applyFilters={this.props.applyFilters} />
 				<br />
 				{this.props.hosts.data.length === 0 && this.props.hosts.loaded && 
-					<Header as='h2'>No data found.</Header>
+					<Heading level="2">No data found.</Heading>
 				}
 				{(this.props.hosts.data.length !== 0 || !this.props.hosts.loaded) &&
 					<div>
-						<Container>
-							<Grid columns={columns}>
-								{hosts_group}
-							</Grid>
-						</Container>
+						<Grid
+							areas={areas}
+							columns={['flex']}
+							rows={rows}
+							gap='small'
+						>
+							{hosts}
+						</Grid>
 						<br />
 						<ReactPaginate
 							pageNumber={this.props.hosts.page}
 							pageCount={this.state.pageCount}
-							clickHandler={this.handlePageClick} />
+							clickHandler={this.handlePageClick}
+						/>
 					</div>
 				}
 			</div>
--- app/src/hosts_list/presentational/HostsEntryLine.jsx
+++ app/src/hosts_list/presentational/HostsEntryLine.jsx
@@ -8,41 +8,49 @@ import {
 	Divider
 } from 'semantic-ui-react'
 
+import {
+	Box,
+	Grid
+} from 'grommet'
+import { Copy, Checkmark} from 'grommet-icons'
+
 import ScopeComment from '../../common/scope_comment/ScopeComment.jsx'
 import HostsEntryLinePorts from './HostsEntryLinePorts.jsx'
 import TasksScoped from '../../common/tasks_scoped/TasksScoped.jsx'
 import Creds from '../../common/creds/Creds.jsx'
+import HidingButtons from '../../ips_list/presentational/scope/HidingButtons.jsx'
 
 
 class HostsEntryLine extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			"copyPasteShown": false,
+			"copySuccess": false
+		};
+
+		this.copyToClipboard = this.copyToClipboard.bind(this);
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return (!_.isEqual(nextProps, this.props)) || (!_.isEqual(this.state, nextState));
+	}
+
+	copyToClipboard(e) {
+		this.hosttext.select();
+		document.execCommand('copy');
+		// This is just personal preference.
+		// I prefer to not show the the whole text area selected.
+		e.target.focus();
+
+		this.setState({
+			"copySuccess": true
+		})
+	};
+
 	render() {
 		const { host, project_uuid, deleteScope, onCommentSubmit } = this.props;
-		const verbose_host_link = '/project/' + project_uuid + '/host/' + host.hostname;
-
-		const footer = (
-			<div>
-				<TasksScoped
-		        	target={host.hostname}
-		        	tasks={host.tasks}
-		        />
-
-	            <a onClick={() => window.open("https://google.com/search?q=site:" + host.hostname, Math.random().toString(36).substring(7))}>
-					<Button basic size="tiny">
-						G Dork
-					</Button>
-	            </a>
-				
-	            <a onClick={() => window.open(verbose_host_link, Math.random().toString(36).substring(7), 'width=850,height=700')}>
-					<Button basic size="tiny">
-						Verbose
-					</Button>
-	            </a>
-
-				<Button basic color="red" size="tiny" onClick={deleteScope}>
-					Delete
-				</Button>
-			</div>
-		);
 
 		let files_by_statuses = {
 			'2xx': 0,
@@ -68,36 +76,73 @@ class HostsEntryLine extends React.Component {
 			}
 		}
 
-
-		const description = (
-			<div>
-				<Header>{host.hostname}</Header>
-				<Divider />
-				<ScopeComment comment={host.comment}
-							  onCommentSubmit={onCommentSubmit} />
-
-				<Creds
-					scope={host}
-					project_uuid={project_uuid}
-				/>
-
-				<Divider hidden />
-				<List bulleted>
-					<List.Item>2xx: <strong>{files_by_statuses['2xx']}</strong></List.Item> 
-					<List.Item>3xx: {files_by_statuses['3xx']}</List.Item>
-					<List.Item>4xx: {files_by_statuses['4xx']}</List.Item>
-					<List.Item>5xx: {files_by_statuses['5xx']}</List.Item>
-				</List>
-
-				<HostsEntryLinePorts host={host} />
-			</div>
-		);
-
 		return (
-			<Card color="blue" fluid>
-				<Card.Content description={description} />
-				<Card.Content extra>{footer}</Card.Content>
-			</Card>			
+			<Box
+				gridArea={"host_" + host.host_id}
+				pad="small"
+				border={{
+					size: "xsmall",
+					color: "neutral-2"
+				}}
+				round="xsmall"
+			>
+				<input
+					ref={(hosttext) => this.hosttext = hosttext}
+					style={{
+						"position": "absolute",
+						"left": "-9999px"
+					}}
+				/>
+				<Grid
+					areas={[
+						{ name: 'host-' + host.hostname, start: [0, 0], end: [0, 0] },
+						{ name: 'comment-' + host.hostname, start: [1, 0], end: [1, 0] },
+						{ name: 'ports-' + host.hostname, start: [2, 0], end: [2, 0] },
+						{ name: 'files-' + host.hostname, start: [3, 0], end: [3, 0] },
+						{ name: 'control-' + host.hostname, start: [4, 0], end: [4, 0] },
+					]}
+					columns={["small", "small", "auto", "small", "xsmall"]}
+					rows={["auto"]}
+				>
+					<Box gridArea={"host-" + host.hostname} direction="row" align="center" gap="small" pad="small">
+						<b
+							onMouseOut={() => this.setState({
+								"copyPasteShown": false,
+								"copySuccess": false
+							})}
+							onMouseOver={() => this.setState({"copyPasteShown": true})}
+							onClick={(e) => {
+								if (document.queryCommandSupported('copy')) {
+									this.hosttext.value = host.hostname;
+									this.copyToClipboard(e);
+								}
+							}}
+							style={{
+								"cursor": "pointer"
+							}}
+						>
+							{host.hostname}
+						</b>
+						{this.state.copyPasteShown && !this.state.copySuccess && <span>  <Copy /></span>}
+						{this.state.copyPasteShown && this.state.copySuccess && <span>  <Checkmark /></span>}
+					</Box>
+					<Box gridArea={"comment-" + host.hostname} direction="row" align="center" gap="small" pad="small">
+						<ScopeComment
+							comment={host.comment}
+							onCommentSubmit={onCommentSubmit}
+						/>
+					</Box>
+					<Box gridArea={"ports-" + host.hostname} />
+					<Box gridArea={"files-" + host.hostname} />
+					<Box gridArea={"control-" + host.hostname}  direction="row" align="center" gap="small" >
+						<HidingButtons
+							project_uuid={project_uuid}
+							type="host"
+							target={host.hostname}
+						/>
+					</Box>
+				</Grid>
+			</Box>
 		)
 	}
 }
