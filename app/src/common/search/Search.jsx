74fac6d5e8d34b1f57677211f992adba0ca3ad99
Anatoly Ivanov <bmth@me.com>
2019-01-24 02:21:17 +0300

    A part of react-select has been prettified

--- app/src/common/search/Search.jsx
+++ app/src/common/search/Search.jsx
@@ -1,7 +1,60 @@
 import _ from 'lodash'
 import React from 'react'
 import Creatable from 'react-select/lib/Creatable'
+import { components } from 'react-select';
 
+import { Box, Text } from 'grommet'
+import { Close } from 'grommet-icons'
+
+
+const SelectContainer = ({ children, ...props }) => {
+	return (
+	  <select>
+		<components.SelectContainer {...props}>
+		  {children}
+		</components.SelectContainer>
+	  </select>
+	);
+  };
+
+const MultiValueContainer = (props) => {
+	return (
+	  <Box
+		background="brand"
+		direction="row"
+		round="xsmall"
+	  >
+		<components.MultiValueContainer {...props} />
+	  </Box>
+	);
+};
+
+const MultiValueLabel = (props) => {
+	return (
+	  <Box
+		background="brand"
+		direction="row"
+	  >
+		<components.MultiValueLabel {...props}>
+			<Text color="light-2">{props.children}</Text>
+		</components.MultiValueLabel>
+	  </Box>
+	);
+};
+
+
+const MultiValueRemove = (props) => {
+	return (
+	  <Box
+		background="brand"
+		direction="row"
+	  >
+		<components.MultiValueRemove {...props}>
+			<Close color="light-2" size="small" />
+		</components.MultiValueRemove>
+	  </Box>
+	);
+};
 
 class Search extends React.Component {
 	constructor(props) {
@@ -61,6 +114,7 @@ class Search extends React.Component {
 			<div className="section">
 				<h3 className="section-heading">{this.props.label}</h3>
 				<Creatable
+					components={{ MultiValueContainer, MultiValueLabel, MultiValueRemove }}
 					isMulti={true}
 					options={options}
 					onChange={this.handleOnChange}
