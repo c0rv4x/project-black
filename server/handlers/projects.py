905932d58e85a52aa5a50937bd821d42067d473b
Anatoly Ivanov <bmth@me.com>
2019-02-05 02:19:15 +0300

    Added handler for creating objects

--- server/handlers/projects.py
+++ server/handlers/projects.py
@@ -6,8 +6,17 @@ class ProjectsHandlers:
     def __init__(self, project_manager):
         self.project_manager = project_manager
 
+
     @authorized_class_method()
     async def cb_get_projects(self, request):
         projects = await self.project_manager.get_projects()
+
+        return response.json(projects)
+
+
+    @authorized_class_method()
+    async def cb_create_project(self, request):
+        name = request.json['name']
+        projects = await self.project_manager.create_project(name)
 
         return response.json(projects)
