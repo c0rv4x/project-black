5d35e4e5e35e61172607222d72e74cc9141cc2a2
Anatoly Ivanov <bmth@me.com>
2019-02-06 01:26:13 +0300

    Removed old methods from ips redux emitter

--- app/src/redux/ips/IPsSocketioEventsEmitter.js
+++ app/src/redux/ips/IPsSocketioEventsEmitter.js
@@ -8,21 +8,6 @@ class IPsSocketioEventsEmitter {
         this.connector = new Connector('ips');
 	}
 
-	requestDeleteIP(ip_id, project_uuid, type) {
-		this.connector.emit('ips:delete:ip_id', {
-			'ip_id': ip_id,
-			'project_uuid': project_uuid,
-			'ip_type': type
-		});
-	}
-
-	requestResolveIPs(ips_ids, project_uuid) {
-		this.connector.emit('ips:resolve', {
-			'ips_ids': ips_ids,
-			'project_uuid': project_uuid
-		});
-	}
-
 	requestUpdateIP(comment, ip_id, project_uuid, ip_type) {
 		this.connector.emit('ips:update', {
 			'ip_id': ip_id,
