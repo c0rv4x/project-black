d4061debe6b566d992a5ac1a92fc96dffec4aaf4
Anatoly Ivanov <bmth@me.com>
2019-02-07 18:52:31 +0300

    Updated hosts subscriber: removed comments and useless variables

--- app/src/redux/hosts/HostsSocketioEventsSubscriber.js
+++ app/src/redux/hosts/HostsSocketioEventsSubscriber.js
@@ -14,8 +14,7 @@ import Connector from '../SocketConnector.jsx';
 
 
 class HostsSocketioEventsSubscriber {
-	/* Singleton class for managing events subscrhosttion for the hosts */
-	constructor(store, project_uuid, hostname=null, filters={}, pageSize=12) {
+	constructor(store, project_uuid) {
         this.store = store;
         this.project_uuid = project_uuid;
         this.connector = new Connector('hosts');
@@ -24,20 +23,16 @@ class HostsSocketioEventsSubscriber {
 	}
 
 	basic_events_registration() {
-		/* Register handlers on basic events */
-		// Received all hosts in one message
 		this.register_socketio_handler('host:comment_updated', hostCommentUpdated);
 		this.register_socketio_handler('hosts:created', hostsCreated);
 		this.register_socketio_handler('host:deleted', hostDeleted);
-
 
 		this.register_socketio_handler('hosts:updated:ips', updatedIPs);
 		this.register_socketio_handler('hosts:resolve:done', resolveHosts);
 
 	}
 
-	register_socketio_handler(eventName, dispatchCallback, callback) {
-		/* Just a wrapper for connector.listen */
+	register_socketio_handler(eventName, dispatchCallback) {
 		this.connector.listen(eventName, (data) => {
 			this.store.dispatch(dispatchCallback(data, this.project_uuid));
 		});
