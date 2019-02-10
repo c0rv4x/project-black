96bd0cccfd56b56bc8a51c39ce6695561df321bd
Anatoly Ivanov <bmth@me.com>
2019-02-11 01:13:17 +0300

    Removed sio handler for creating tasks

--- events_handling/tasks_handlers.py
+++ events_handling/tasks_handlers.py
@@ -20,35 +20,6 @@ class TaskHandlers(object):
 
             await self.send_tasks_back(project_uuid, send_all=send_all)
 
-        @self.socketio.on('tasks:create', namespace='/tasks')
-        async def _cb_handle_tasks_create(sio, msg):
-            """ When received this message, create a new tasks """
-            task_type = msg["task_type"]
-            filters = msg["filters"]
-            params = msg["params"]
-            project_uuid = msg["project_uuid"]
-
-            tasks = self.task_manager.create_task(
-                task_type, filters, params, project_uuid
-            )
-
-            await self.socketio.emit(
-                'tasks:new',
-                {
-                    'status': 'success',
-                    'new_tasks': tasks
-                },
-                namespace='/tasks'
-            )
-
-            await send_notification(
-                self.socketio,
-                "success",
-                "Tasks created",
-                "Created {} {} tasks".format(len(tasks), task_type),
-                project_uuid=project_uuid
-            )
-
     async def send_tasks_back(self, project_uuid=None, send_all=False):
         """ Grab all tasks data and send them back to client """
         if send_all:
