91c70fbaca9706ae33d99a58a7262b19ca80ce07
Anatoly Ivanov <bmth@me.com>
2019-02-11 01:26:59 +0300

    Removed sio handler for getting tasks

--- events_handling/tasks_handlers.py
+++ events_handling/tasks_handlers.py
@@ -9,47 +9,22 @@ class TaskHandlers(object):
         self.socketio = socketio
         self.task_manager = task_manager
 
-        self.register_handlers()
-
-    def register_handlers(self):
-        @self.socketio.on('tasks:all:get', namespace='/tasks')
-        async def _cb_handle_tasks_get(sio, msg):
-            """ When received this message, send back all the tasks """
-            project_uuid = int(msg.get('project_uuid', None))
-            send_all = msg.get('send_all')
-
-            await self.send_tasks_back(project_uuid, send_all=send_all)
 
     async def send_tasks_back(self, project_uuid=None, send_all=False):
         """ Grab all tasks data and send them back to client """
-        if send_all:
-            tasks = self.task_manager.get_tasks(
-                project_uuid, only_fresh=False
-            )
+        tasks = self.task_manager.get_tasks(
+            project_uuid, only_fresh=True
+        )
 
-            await self.socketio.emit(
-                'tasks:all:get:back:all',
-                {
-                    "status": "success",
-                    "project_uuid": project_uuid,
-                    "tasks": tasks
-                },
-                namespace='/tasks'
-            )
-        else:
-            tasks = self.task_manager.get_tasks(
-                project_uuid, only_fresh=True
-            )
+        if len(tasks['finished']) == 0 and len(tasks['active']) == 0:
+            return
 
-            if len(tasks['finished']) == 0 and len(tasks['active']) == 0:
-                return
-
-            await self.socketio.emit(
-                'tasks:all:get:back:updated',
-                {
-                    "status": "success",
-                    "project_uuid":  project_uuid,
-                    "tasks": tasks
-                },
-                namespace='/tasks'
-            )
+        await self.socketio.emit(
+            'tasks:all:get:back:updated',
+            {
+                "status": "success",
+                "project_uuid":  project_uuid,
+                "tasks": tasks
+            },
+            namespace='/tasks'
+        )
