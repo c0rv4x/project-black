c6e6f117070bea73e1d49cfac17e7d25b0acdb88
Anatoly Ivanov <bmth@me.com>
2019-02-11 04:11:36 +0300

    Removed requestFilesHosts from files emitter

--- app/src/redux/files/FilesSocketioEventsEmitter.js
+++ app/src/redux/files/FilesSocketioEventsEmitter.js
@@ -7,17 +7,6 @@ class FilesSocketioEventsEmitter {
         this.connector = new Connector('files');
     }
 
-    requestFilesHosts(project_uuid, host, port_number, limit, offset, filters) {
-        this.connector.emit('files:get:hosts', {
-            "project_uuid": project_uuid,
-            "host": host,
-            "port_number": port_number,
-            "limit": limit,
-            "offset": offset,
-            "filters": filters
-        });
-    }
-
     requestFilesIps(project_uuid, ip, port_number, limit, offset, filters) {
         this.connector.emit('files:get:ips', {
             "project_uuid": project_uuid,
